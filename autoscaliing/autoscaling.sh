40c24eedc1dc39ff2bc5e687d1b111dae1b99232
Peter Delaney <delaneymichaelpeter@gmail.com>
2017-11-15 06:44:48 -0500

    happy with changes

--- autoscaliing/autoscaling.sh
+++ autoscaliing/autoscaling.sh
@@ -6,15 +6,25 @@ echo "Building Environment SecurityGroup ELB with AutoScaling"
 # delaneyskivt userid
 export AWS_PROFILE=delaneyskivt
 
+# Important variable names
+keyPair=pdelaney
 vpcName=Default-VPC
+httpSgName=delaney-HTTP-SG
+sshSgName=delaney-SSH-SG
+elbName=delaney-ELB
+myIp=$(curl -sS http://checkip.amazonaws.com/)
+echo "MY IP : $myIp"
+
+
 # Get the Default VPC; assumes have one and default is the first returned.  NOT GOOD
 vpcId=`aws ec2 describe-vpcs --output text --query 'Vpcs[0].VpcId'`
+echo "Using VPC : $vpcId"
+
 # Another way
-vpcId=`aws ec2 describe-vpcs --filters Name=tag-key,Values=vpcname --filters Name=tag-value,Values=$vpcName --output text --query 'Vpcs[*].VpcId'`
-echo "VPC : $vpcId"
+#vpcId=`aws ec2 describe-vpcs --filters Name=tag-key,Values=vpcname --filters Name=tag-value,Values=$vpcName --output text --query 'Vpcs[*].VpcId'`
+#echo "Described VPC : $vpcId"
 
 # Create Security Group for HTTP and SSH
-httpSgName=delaney-HTTP-SG
 httpSgId=`aws ec2 create-security-group --group-name $httpSgName --description "Security to allow HTTP traffic" --vpc-id $vpcId --output text --query 'GroupId'`
 aws ec2 create-tags --resources $httpSgId --tags Key=Name,Value="HTTP to EC2"
 aws ec2 authorize-security-group-ingress --group-name $httpSgName --protocol tcp --port 80 --cidr 0.0.0.0/0
@@ -22,62 +32,98 @@ echo "Http Security Group : $httpSgId"
 
 
 # Create SSH
-sshSgName=delaney-SSH-SG
 sshSgId=`aws ec2 create-security-group --group-name $sshSgName --description "Allows SSH to box" --vpc-id $vpcId --output text --query 'GroupId'`
 aws ec2 create-tags --resources $sshSgId --tags Key=Name,Value="SSH to EC2"
 aws ec2 authorize-security-group-ingress --group-name $sshSgName --protocol tcp --port 22 --cidr 0.0.0.0/0
+#aws ec2 authorize-security-group-ingress --group-name $sshSgName --protocol tcp --port 22 --cidr $myIp/32
 echo "SSH Security Group : $sshSgId"
 
 
 # Create Load Balancer
-elbName=delaney-ELB
 subnetIds=`aws ec2 describe-subnets --filters Name=vpc-id,Values=$vpcId --output text --query 'Subnets[*].SubnetId'`
+subnetId=`aws ec2 describe-subnets --filters Name=vpc-id,Values=$vpcId --output text --query 'Subnets[0].SubnetId'`
 elbDnsName=`aws elb create-load-balancer --load-balancer-name $elbName --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --subnets $subnetIds --security-groups $sshSgId --output text --query 'DNSName'`
 echo "ELB DNS Name : $elbDnsName"
 
-# Delete Resources thus far
+
+# Create Instance that will be our launch configuration
+instanceType=t2.micro
+aZone=us-east-1a
+imageId=ami-6057e21a
+
+instanceId=`aws ec2 run-instances --image $imageId --key $keyPair --security-group-ids $sshSgId $httpSgId --count 1 --instance-type $instanceType --user-data file://user_data.txt --subnet-id $subnetId --output text --query 'Instances[*].InstanceId'`
+echo "Create EC2 Instance : $instanceId for Launch Configuration"
+
+# Wait for instance to be in running state
+echo "Wait for Instance to be running"
+while state=$(aws ec2 describe-instances --instance-ids $instanceId --output text --query 'Reservations[*].Instances[*].State.Name'); test "$state" = "pending"; do
+    echo -n . ; sleep 3;
+done;
+echo "Instance State $state"
+
+
+
+# Create Launch Configuration from EC2 instance
+# Create Launch Configuration from EC2 and Override Instance Type
+launchConfigName=delaney-launch
+aws autoscaling create-launch-configuration --launch-configuration-name $launchConfigName --instance-id $instanceId --instance-type $instanceType
+
+# Describe your launch configuration
+echo "#############################################"
+echo "Created Launch Configuration : $launchConfigName"
+aws autoscaling describe-launch-configurations --launch-configuration-names $launchConfigName
+echo "#############################################"
+
+
+# Create AutoScaling Group
+asgName=delaney-ASG
+
+subnetNames=`aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpcId" --output text --query 'Subnets[*].AvailabilityZone'`
+echo "subnet Names : $subnetNames"
+# only takes one subnet, not sure why
+firstSubnetId=`aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpcId" --output text --query 'Subnets[0].SubnetId'`
+echo "subnet ids : $firstSubnetId"
+
+# Go to sleep seems to have problems
+sleep 10; 
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name $asgName --launch-configuration-name $launchConfigName --vpc-zone-identifier $firstSubnetId --load-balancer-names $elbName --max-size 0 --min-size 0 --desired-capacity 0 --health-check-type ELB --health-check-grace-period 120
+
+echo "Created AutoScaling Group : $asgName"
+
+
+# Delete Resources thus far sleep/wait for resources to get established
+aws ec2 terminate-instances --instance-ids $instanceId
+# Wait for instance to be in running state
+echo "Wait for Instance to be terminated"
+while state=$(aws ec2 describe-instances --instance-ids $instanceId --output text --query 'Reservations[*].Instances[*].State.Name'); test "$state" = "shutting-down"; do
+    echo -n . ; sleep 3;
+done;
+echo "Instance State $state"
+
+echo "Delete AutoScaling Group : $launchName"
+aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $asgName
+
+
+echo "Delete AutoScaling Launch Configuration : $launchName"
+aws autoscaling delete-launch-configuration --launch-configuration-name $launchConfigName
+
+echo "Deleted instanceId : $instanceId"
 aws elb delete-load-balancer  --load-balancer-name $elbName
-echo "Deleted ELB $elbName"
-aws ec2 delete-security-group --group-name $sshSgName
-echo "Deleted Security Group $sshSgName"
+echo "Deleted ELB $elbName : $elbName"
+
+sleep 10;
 aws ec2 delete-security-group --group-name $httpSgName
-echo "Deleted Security Group $httpSgName"
+echo "Deleted Security Group : $httpSgName"
+
+aws ec2 delete-security-group --group-name $sshSgName
+echo "Deleted Security Group : $sshSgName"
+
 
 
 # Stop Here
 exit 1
 
-# Create Launch Configuration from EC2 instance
-aws autoscaling create-launch-configuration --launch-configuration-name delaney-launch --instance-id i-a8e09d9c
 
-# Describe your launch configuration
-aws autoscaling describe-launch-configurations --launch-configuration-names delaney-launch
-
-# Create Launch Configuraiton from EC2 instance but Override the block device
-aws autoscaling create-launch-configuration --launch-configuration-name delaney-launch --instance-id i-a8e09d9c \
---block-devic-mappings "[{\"DeviceName\":\"/dev/sda1\",\"Ebs\":{"SnapshotId\":\"snap-3decf207\"}},{\"DeviceName\":\"/dev/sdf\",\"Ebs\":{"SnapshotId\":\"snap-eed6ac86\"}}]"
-
-# Create Launch Configuration from EC2 and Override Instance Type
-aws autoscaling create-launch-configuration --launch-configuration-name delaney-launch --instance-id i-a8e09d9c \
---instance-type m1.small
-
-# Create Launch Configuration from EC2 and Override Instance Type and set Spot Price
-aws autoscaling create-launch-configuration --launch-configuration-name delaney-launch --image-id ami-1a2bc4d \
---instance-type m1.small --spot-price "0.05"
-
-
-# Create AutoScaling Group
-asgName=delaney-ASG
-subnetNames=`aws ec2 describe-subnets --filters Name=vpc-id,Values=$vpcId --output text --query 'Subnets[*].AvailabilityZones'`
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name $asgName --launch-configuration-name delaney-launch --availability-zones $subnetNames \
---load-balancer-names $elbName --max-size 5 --min-size 1 --desired-capacity 2
-
-
-
-# Create AutoScaling Group from a running EC2 Instance
-aws autoscaling create-scaling-group --auto-scaling-group-name delaney-asg-cli \
---instance-id i-7f12e649 \
---min-size 1 --max-size 2 --desired-capacity 2
 
 # Add Tags to Existing AutoScaling Group
 aws autoscaling create-or-update-tags --tags "ResourceId=my-asg,ResourceType=auto-scaling-group,Key=environment,Value=test,PropagateAtLaunch=true"
